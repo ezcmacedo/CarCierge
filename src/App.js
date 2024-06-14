db77af9ba190c682b7468cca41bf7f7f443e3ff3
Enzo <ezcmacedo@gmail.com>
2024-06-13 23:55:54 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import { Outlet } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import 
 
 function App() {
+
   return (
-    <div className="App">
+    <div className="h-hidden">
       
       <Header></Header>
       <Outlet />
